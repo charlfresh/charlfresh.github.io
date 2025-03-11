edc99c536633c93b518034a2ad7e47d51a778a31
Charlie Blanchard <charles.blanchard09@icloud.com>
2025-03-11 14:20:33 -0400

    Self

---
+++ canvas/script.js
@@ -0,0 +1,208 @@
+const canvas = document.getElementById('myCanvas')
+const ctx = canvas.getContext('2d')
+
+ctx.beginPath();
+ctx.arc(250, 150, 50, 0, Math.PI * 2);
+ctx.fillStyle = '#F5F5DC';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(200, 225, 100, 100);
+ctx.fillStyle = '#000';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(300, 225, 25, 50);
+ctx.fillStyle = '#000';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(175, 225, 25, 50);
+ctx.fillStyle = '#000';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(300, 275, 25, 25);
+ctx.fillStyle = '#F5F5DC';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(175, 275, 25, 25);
+ctx.fillStyle = '#F5F5DC';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(200, 325, 45, 75);
+ctx.fillStyle = '#4682b4';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(255, 325, 45, 75);
+ctx.fillStyle = '#4682b4';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(250, 250, 50, 0, Math.PI * 2);
+ctx.fillStyle = '#000';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(230, 140, 7, 0, Math.PI * 2);
+ctx.arc(270, 140, 7, 0, Math.PI * 2);
+ctx.fillStyle = '#FFFFFF';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(223, 130);
+ctx.lineTo(237, 130);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(263, 130);
+ctx.lineTo(277, 130);
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.arc(230, 140, 3, 0, Math.PI * 2);
+ctx.arc(270, 140, 3, 0, Math.PI * 2);
+ctx.fillStyle = '#6c8490';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(230, 140, 7, 0, Math.PI * 2);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(270, 140, 7, 0, Math.PI * 2);
+ctx.stroke();
+ctx.closePath();
+
+
+
+ctx.beginPath();
+ctx.arc(250, 160, 30, 0.2 * Math.PI, 0.8 * Math.PI);
+ctx.fillStyle = '#FFFFFF';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(250, 160, 30, 0.2 * Math.PI, 0.8 * Math.PI); 
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.moveTo(225, 178);
+ctx.lineTo(275, 178);
+ctx.stroke();
+ctx.fillStyle = '#000';
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(230, 183);
+ctx.lineTo(270, 183);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(232, 178);
+ctx.lineTo(232, 183);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(268, 178);
+ctx.lineTo(268, 183);
+ctx.stroke(); 
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(237, 178);
+ctx.lineTo(237, 187);
+ctx.stroke();
+ctx.closePath();
+ctx.beginPath();    
+ctx.moveTo(242, 178);
+ctx.lineTo(242, 189.5);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();    
+ctx.moveTo(263, 178);
+ctx.lineTo(263, 187);
+ctx.stroke();
+ctx.closePath();
+ctx.beginPath();    
+ctx.moveTo(258, 178);
+ctx.lineTo(258, 189.5);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();    
+ctx.moveTo(247, 178);
+ctx.lineTo(247, 190);
+ctx.stroke();
+ctx.closePath();
+ctx.beginPath();    
+ctx.moveTo(253, 178);
+ctx.lineTo(253, 190);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(250, 155);
+ctx.lineTo(247, 162);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(250, 155);
+ctx.lineTo(253, 162);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(250, 100, 7.5, 0, Math.PI * 2);
+ctx.arc(240, 108, 7.5, 0, Math.PI * 2);
+ctx.arc(258, 93, 7.5, 0, Math.PI * 2);
+ctx.arc(234, 101, 7.5, 0, Math.PI * 2);
+ctx.arc(265, 99, 7.5, 0, Math.PI * 2);
+ctx.arc(220, 95, 7, 0, Math.PI * 2);
+ctx.arc(280, 118, 8, 0, Math.PI * 2);
+ctx.arc(240, 108, 10, 0, Math.PI * 2);
+ctx.arc(260, 109, 6, 0, Math.PI * 2);
+ctx.arc(225, 106, 12, 0, Math.PI * 2);
+ctx.arc(276, 96, 5, 0, Math.PI * 2);
+ctx.arc(210, 119, 9, 0, Math.PI * 2);
+ctx.arc(230, 97, 11, 0, Math.PI * 2);
+ctx.arc(270, 112, 6, 0, Math.PI * 2);
+ctx.arc(260, 106, 8, 0, Math.PI * 2);
+ctx.arc(290, 116, 7, 0, Math.PI * 2);
+ctx.arc(250, 92, 10, 0, Math.PI * 2);
+ctx.arc(240, 100, 5, 0, Math.PI * 2);
+ctx.arc(215, 118, 9, 0, Math.PI * 2);
+ctx.arc(290, 113, 11, 0, Math.PI * 2);
+ctx.arc(270, 110, 12, 0, Math.PI * 2);
+ctx.arc(210, 108, 6, 0, Math.PI * 2);
+ctx.arc(280, 120, 8, 0, Math.PI * 2);
+ctx.arc(220, 106, 5, 0, Math.PI * 2);
+ctx.arc(250, 105, 9, 0, Math.PI * 2);
+ctx.fillStyle = '#964B00';
+ctx.fill();
+ctx.closePath();
+
